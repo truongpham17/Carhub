143ef86501e1b66ffae0918108c461ad583cdda1
Akyrisk <trivu27.97@gmail.com>
2020-02-24 15:16:48 +0700

    continue initial project

---
+++ src/screens/rent-history-screen/RentHistoryScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { ViewContainer } from 'components';
+
+const RentHistoryScreen = () => (
+  <ViewContainer>
+    <View />
+  </ViewContainer>
+);
+
+export default RentHistoryScreen;
